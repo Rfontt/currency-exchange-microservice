61f9ae9bb8c136ee901a9b66248b354931267f00
Rfontt <ritafontenele@ext.cvccorp.com.br>
2022-05-25 19:08:17 -0300

    Setting up environment

---
+++ src/main/java/com/microservice/currencyexchange/CurrecyExchangeApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.currencyexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrecyExchangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrecyExchangeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/microservice/currencyexchange/CurrecyExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.currencyexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrecyExchangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
